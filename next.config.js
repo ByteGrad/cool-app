ceec94c9c8125a5b2eece5559b5df02271dd2f2f
ByteGrad <bytegrad@gmail.com>
2023-11-12 22:40:03 +0100

    save

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["62.72.26.118", "localhost:3000"],
+      allowedForwardedHosts: ["62.72.26.118", "localhost:3000"],
+    },
+  },
   images: {
     remotePatterns: [
       {
