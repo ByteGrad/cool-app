e435d33f53309a04b4683e97397b0f726789dd45
ByteGrad <bytegrad@gmail.com>
2023-11-13 14:01:45 +0100

    save

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,27 +5,18 @@ const prisma = new PrismaClient();
 const posts = [
   {
     id: 1,
-    title: "His mother had always taught him",
-    body: "His mother had always taught him not to ever think of himself as better than others. He'd tried to live by this motto. He never looked down on those who were less fortunate or who had less money than him. But the stupidity of the group of people he was talking to made him change his mind.",
-    userId: 9,
-    tags: ["history", "american", "crime"],
-    reactions: 2,
+    title: "Just a random post",
+    body: "This is an awesome post, isn't it?",
   },
   {
     id: 2,
-    title: "He was an expert but not in a discipline",
-    body: "He was an expert but not in a discipline that anyone could fully appreciate. He knew how to hold the cone just right so that the soft server ice-cream fell into it at the precise angle to form a perfect cone each and every time. It had taken years to perfect and he could now do it without even putting any thought behind it.",
-    userId: 13,
-    tags: ["french", "fiction", "english"],
-    reactions: 2,
+    title: "Another cool post",
+    body: "Super cool post!",
   },
   {
     id: 3,
-    title: "Dave watched as the forest burned up on the hill.",
-    body: "Dave watched as the forest burned up on the hill, only a few miles from her house. The car had been hastily packed and Marta was inside trying to round up the last of the pets. Dave went through his mental list of the most important papers and documents that they couldn't leave behind. He scolded himself for not having prepared these better in advance and hoped that he had remembered everything that was needed. He continued to wait for Marta to appear with the pets, but she still was nowhere to be seen.",
-    userId: 32,
-    tags: ["magical", "history", "french"],
-    reactions: 5,
+    title: "Amazing post",
+    body: "This is a great post for sure.",
   },
 ];
 
--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,5 +1,8 @@
+// "use client";
+
 import prisma from "@/lib/db";
 import { notFound } from "next/navigation";
+import SyntaxHighlighter from "react-syntax-highlighter";
 
 type PostPageProps = {
   params: {
@@ -7,24 +10,27 @@ type PostPageProps = {
   };
 };
 
-export default async function PostPage({ params }: PostPageProps) {
+export default function PostPage({ params }: PostPageProps) {
   // const response = await fetch(`https://dummyjson.com/posts/${params.id}`);
   // const data = await response.json();
 
-  const post = await prisma.post.findUnique({
-    where: {
-      id: +params.id,
-    },
-  });
+  // const post = await prisma.post.findUnique({
+  //   where: {
+  //     id: +params.id,
+  //   },
+  // });
 
-  if (!post) {
-    return notFound();
-  }
+  // if (!post) {
+  //   return notFound();
+  // }
 
   return (
     <main className="px-7 pt-24 text-center">
-      <h1 className="text-5xl font-semibold mb-7">{post.title}</h1>
-      <p className="max-w-[700px] mx-auto">{post.body}</p>
+      {/* <h1 className="text-5xl font-semibold mb-7">{post.title}</h1>
+      <p className="max-w-[700px] mx-auto">{post.body}</p> */}
+      <SyntaxHighlighter className="text-left">{`function() {
+  return "Hello World!";
+}`}</SyntaxHighlighter>
     </main>
   );
 }
